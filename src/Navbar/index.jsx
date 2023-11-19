b55ae5907487d0da1c6112de7740f76b933d3320
pomff <105871459+pomfuu@users.noreply.github.com>
2023-11-19 23:14:02 +0700

    react6

--- src/Navbar/index.jsx
+++ src/Navbar/index.jsx
@@ -1,14 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+
+    const navigate = useNavigate();
+    const accessToken = localStorage.getItem("accessToken")
+    console.log("token", accessToken)
+
+    const logout = () => {
+        localStorage.removeItem("accessToken")
+        // navigate("/login")
+        // localStorage.clear
+    }
+    
+    const handleClick = () => {
+        if (accessToken) {
+            localStorage.removeItem("accessToken");
+            navigate('/');
+        } else {
+            navigate('/login');
+        }
+    };
+    
     return (
         <div className="col d-flex gap-5 p-4 mb-4" style={{ backgroundColor:"#1e1e1e" }}>
             <Link className="text-decoration-none text-white" to={"/"}>
                 <p>Home</p>
             </Link>
-            <Link className="text-decoration-none text-white" to={"/login"}>
-                <p>Login</p>
+            <Link onClick={() => accessToken ? navigate('/') : navigate('/login')} className="text-decoration-none text-white">
+                <p> {accessToken ? "Logout" : "Login"} </p>
             </Link>
+            { accessToken? 
+                (<p onClick={logout}>Logout</p> ) : ( <Link><p>Login</p></Link> )
+            }
         </div>
     )
 }
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,19 +1,77 @@
+import { useState } from "react";
 import Navbar from "../../Navbar";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    const navigate = useNavigate()
+
+    const onChangeUsername = (event) => {
+        setUsername(event.target.value)
+        console.log(event.target.value)
+        setError('')
+        setSuccess('')
+    }
+
+    const onChangePassword = (event) => {
+        setPassword(event.target.value)
+        console.log(event.target.value)
+        setError('')
+        setSuccess('')
+    }
+
+    const onSubmit = (e) =>{
+        e.preventDefault()
+        setLoading(true)
+
+        const payload = {
+            username: username,
+            password: password,
+        }
+        console.log(payload)
+        
+        axios
+        .post("https://api.mudoapi.tech/login", payload)
+        .then((res)=>{
+            const token = res.data.data.token
+            localStorage.setItem("accessToken", token)
+            console.log(res)
+            setSuccess(res.data.message)
+            
+            navigate("/")
+        })
+        .catch((error)=>{
+            console.log(error.response)
+            setError(error.response.data.message)
+        })
+        .finally(()=>{
+            setLoading(false)
+        })
+    }
+    console.log(loading)
+    
     return (
         <div>
             <Navbar/>
-            <form action="">
+            {error.length ? <h1> {error} </h1> : null}
+            {success.length ? <h1> {success} </h1> : null}
+            <form method="POST">
                 <div>
-                    <label htmlFor="">Email</label>
-                    <input type="email" />
+                    <label>Username</label>
+                    <input onChange={onChangeUsername} type="text" />
                 </div>
                 <div>
-                    <label htmlFor="">Password</label>
-                    <input type="password" />
+                    <label>Password</label>
+                    <input onChange={onChangePassword} type="password" />
                 </div>
-                <button className="btn btn-primary">Submit</button>
+                <button disabled= { loading ? true : false } onClick={onSubmit} className="btn btn-primary"> { loading? "loading...." : "Submit" } </button>
             </form>
         </div>
     )
